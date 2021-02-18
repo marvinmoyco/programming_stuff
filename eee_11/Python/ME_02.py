4d534b0e224055e9d69549b92d223cabab986fbe
marvinmoyco <marvin.moyco@gmail.com>
2021-02-19 00:05:42 +0800

    Added completed machine exercise 2 in python

--- eee_11/Python/ME_02.py
+++ eee_11/Python/ME_02.py
@@ -3,6 +3,7 @@
 
 
 import math
+import datetime
 
 def main():
     check = True
@@ -23,11 +24,30 @@ def main():
         else:
             print("The integer {} is zero!".format(number))
     elif mode_input == 2: # Mode 2 - Simple Questionnaire
-        pass
+        print("Mode selected: Simple Questionnaire...")
+        answer = input("Question: Who is the Miss Universe 2017 winner?\n a. Philippines\nb. France\nc. Netherlands\nd. Haiti ").lower()
+        if answer == "b":
+            print("You have chosen the correct answer!")
+        elif answer == "a" or answer == "c" or answer == "d":
+            print("You have chosen an incorrect answer.")
+        else:
+            print("Invalid input...")
     elif mode_input == 3: # Mode 3 - Month Converter
-        pass
+        print("Mode selected: Month Converter...")
+        month_number = int(input("Choose a month number (1-12): "))
+        if month_number >=1 and month_number <= 12:
+            month_string = datetime.datetime(2021,month_number,1) # Kinda cheating.. I am using datetime class fo python hehe
+            print("The number {} is the month of {}.".format(month_number,month_string.strftime("%B")))
+        else:
+            print("Invalid input....")
     elif mode_input == 4: # Mode 4 - Vowel Identifier
-        pass
+        print("Mode selected: Vowel Identifier...")
+        letter = input("Select a letter: ").lower()
+        if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+            print("The letter {} is a vowel.".format(letter))
+        else: 
+            print("The letter {} is a consonant".format(letter))
+
 
 if __name__ == "__main__":
     main()
